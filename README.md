a06ff66e8deb8778e9b68a55ff2a125df408655f
tryingfly <39267563+tryingfly@users.noreply.github.com>
2024-03-05 21:04:38 +0800

    0
    0

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ini-reader
-an good way to replace yaml, xml
+
+A good choice to replace yaml, xml or json for robotics with c++.
+
+Lite and easy to use.
+
+Copyright (C) 2024 YYP, Shanghai Jiao Tong University, under MIT License.
+
+https://zhuanlan.zhihu.com/p/685164920
---
+++ ini.h
@@ -0,0 +1,34 @@
+/*=========== ***doc description @ yyp*** ===========
+Copyright (C) 2024 YYP, Shanghai Jiao Tong University, under MIT License.
+<https://github.com/tryingfly/ini-reader>
+<https://zhuanlan.zhihu.com/p/685164920>
+
+读取解析配置文件*.ini
+不要在实时循环使用，可以在进实时循环前先找变量赋值
+不能在全局变量使用
+可以在类构造函数中使用(main之前执行)，但该类不能实例化全局变量
+#include"ini.h"
+=====================================================*/
+#pragma once
+#include<string>
+using namespace std;
+
+namespace Ini{
+//不要在实时循环使用
+class iniClass{
+public:
+	iniClass();//不要在实时循环使用
+	iniClass(string fileName);//不要在实时循环使用
+	double operator[](string key);//不要在实时循环使用
+	string getStr(string key);//不要在实时循环使用
+	template<typename T,int n>
+	void getArray(string key,T (&value)[n]){getArray(key,value,n);};//不要在实时循环使用
+	template<typename T>
+	void getArray(string key,T *value,int n);//不要在实时循环使用
+private:
+	class impClass;
+	impClass&imp;
+};
+}//namespace
+
+extern Ini::iniClass ini;//已实例化，默认打开“000.ini”
