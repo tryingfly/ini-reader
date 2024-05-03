21ea59bce3de9fa4586c946ef2dac311382c3e47
tryingfly <39267563+tryingfly@users.noreply.github.com>
2024-05-03 10:27:45 +0800

    支持换行

--- ini.cpp
+++ ini.cpp
@@ -12,6 +12,7 @@ Copyright (C) 2024 YYP, Shanghai Jiao Tong University, under MIT License.
 #include<fstream>
 
 namespace Ini{
+	//按token分割
 	inline void splitString(string &str, vector<string> &substrings, char token){
 		string tmp;
 		stringstream ss(str);
@@ -19,126 +20,164 @@ namespace Ini{
 			substrings.push_back(tmp);
 		}
 	}
+	//去除空格、tab
+	void wipeWhite(string &str){
+		size_t id=0;
+		while((id=str.find(' ',id)) != string::npos){
+			str.erase(id,1);
+		}
+		id=0;
+		while((id=str.find('\t',id)) != string::npos){
+			str.erase(id,1);
+		}
+	}
 //==========================
 class iniClass::impClass{
 public:
 	impClass();
-	impClass(string fileName);
+	impClass(const string &fileName);
 	ifstream f;
 	map<string,string>mp;
 	
 	void init();
-	double operator[](string key);
-	string getStr(string key);
+	double operator[](const string &key);
+	const string &getStr(const string &key);
+
 	template<typename T,int n>
-	void getArray(string key,T (&value)[n]){getArray(key,value,n);};
+	void getArray(const string &key,T (&value)[n]){getArray(key,value,n);};
 	template<typename T>
-	void getArray(string key,T *value,int n);
+	void getArray(const string &key,T *value,int n);
 };
 	iniClass::impClass::impClass(){
 		#ifdef _WIN32
 		f.open("../../000.ini");
+		if(f.is_open()){fout.open("../../zzz.txt", ios::trunc);}
 		#else
 		f.open("../000.ini");
+		if(f.is_open()){fout.open("../zzz.txt", ios::trunc);}
 		#endif
 		if(!f.is_open()){
 			f.open("000.ini");
+			fout.open("zzz.txt", ios::trunc);
 		}
 		init();
 	}
-	iniClass::impClass::impClass(string fileName){
+	iniClass::impClass::impClass(const string &fileName){
 		f.open(fileName);
 		init();
 	}
 	void iniClass::impClass::init(){
 		string line;
+		string preKey;
 		while(getline(f,line)){
 			size_t id=0;
-			//去除空格
-			while((id=line.find(' ',id)) != string::npos){
-				line.erase(id,1);
-			}
-			//去除tab
-			id=0;
-			while((id=line.find('\t',id)) != string::npos){
-				line.erase(id,1);
-			}
 			//分号是ini的注释标志
 			id=line.find(";");
-			if(id != string::npos){
+			if(id!=string::npos){
 				line=line.substr(0,id);
 			}
+			wipeWhite(line);
+			if(line.size()==0){
+				continue;
+			}
 			id=line.find("=");
-			if(id != string::npos){
+			if(id==string::npos){//无“=”号，视为上一行的换行
+				if(preKey.size()>0){
+					auto &value=mp[preKey];
+					if(value[value.size()-1]!=',' && line[0]!=','){
+						stringstream ss;
+						ss<<"ini：跨行数组拼接，未发现逗号分割符！key="<<preKey;
+						cout<<ss.str()<<endl;
+						throw runtime_error(ss.str());
+					}else{
+						value+=line;
+					}
+				}
+			}else{//有“=”号，新键值对
 				string key,value;
 				key.assign(line,0,id);
 				value.assign(line,id+1,line.size()-id);
 				mp.insert(pair<string,string>(key,value));
+				preKey=key;
 			}
 		}
 	}
-	double iniClass::impClass::operator[](string key){
+	double iniClass::impClass::operator[](const string &key){
 		auto it=mp.find(key);
 		if(it!=mp.end()){
 			try{
 				return stod(it->second);
 			}catch(...){
-				cout<<"\" ini[\""<<key<<"\"] \" 值类型不匹配"<<endl;
-				throw runtime_error("\" ini[\""+key+"\"] \" 值类型不匹配");
+				stringstream ss;
+				ss<<"\" ini[\""<<key<<"\"] \" 值类型不匹配";
+				cout<<ss.str()<<endl;
+				throw runtime_error(ss.str());
 			}
 		}else{
-			cout<<"\" ini[\""<<key<<"\"] \" 找不到key"<<endl;
-			throw runtime_error("\" ini[\""+key+"\"] \" 找不到key");
+			stringstream ss;
+			ss<<"\" ini[\""<<key<<"\"] \" 找不到key";
+			cout<<ss.str()<<endl;
+			throw runtime_error(ss.str());
 		}
+
+
 	};
-	string iniClass::impClass::getStr(string key){
+	const string &iniClass::impClass::getStr(const string &key){
 		auto it=mp.find(key);
 		if(it!=mp.end()){
 			return it->second;
 		}else{
-			cout<<"\" ini[\""<<key<<"\"] \" 找不到key"<<endl;
-			throw runtime_error("\" ini[\""+key+"\"] \" 找不到key");
+			stringstream ss;
+			ss<<"\" ini[\""<<key<<"\"] \" 找不到key";
+			cout<<ss.str()<<endl;
+			throw runtime_error(ss.str());
 		}
 	};
 	template<typename T>
-	void iniClass::impClass::getArray(string key,T *value,int n){
+	void iniClass::impClass::getArray(const string &key,T *value,int n){
 		auto it=mp.find(key);
 		if(it!=mp.end()){
 			std::vector<std::string> vs;
 			splitString(it->second,vs,',');
 			if(vs.size()<n){
-				cout<<"ini.getArray数目不匹配，key="+key<<endl;
-				throw runtime_error("ini.getArray数目不匹配，key="+key);
+				stringstream ss;
+				ss<<"\"ini.getArray\" 数目不匹配，key="<<key;
+				cout<<ss.str()<<endl;
+				throw runtime_error(ss.str());
 			}
 			try{
 				for(int i=0;i<n;i++){
 					value[i]=(T)stod(vs[i]);
 				}
 			}catch(...){
-				cout<<"ini.getArray值类型不匹配，key="+key<<endl;
-				throw runtime_error("ini.getArray值类型不匹配，key="+key);
+				stringstream ss;
+				ss<<"\"ini.getArray\" 值类型不匹配，key="<<key;
+				cout<<ss.str()<<endl;
+				throw runtime_error(ss.str());
 			}
 		}else{
-			cout<<"ini.getArray找不到key="+key<<endl;
-			throw runtime_error("ini.getArray找不到key="+key);
+			stringstream ss;
+			ss<<"\"ini.getArray\" 找不到key="<<key;
+			cout<<ss.str()<<endl;
+			throw runtime_error(ss.str());
 		}
 	}
 //==========================
 	iniClass::iniClass():imp(*new impClass()){}
-	iniClass::iniClass(string fileName):imp(*new impClass(fileName)){}
-	double iniClass::operator[](string key){
+	iniClass::iniClass(const string &fileName):imp(*new impClass(fileName)){}
+	double iniClass::operator[](const string &key){
 		return imp[key];
 	};
-	string iniClass::getStr(string key){
+	const string &iniClass::getStr(const string &key){
 		return imp.getStr(key);
 	};
 	template<typename T>
-	void iniClass::getArray(string key,T *value,int n){
+	void iniClass::getArray(const string &key,T *value,int n){
 		imp.getArray(key,value,n);
 	}
-	template void iniClass::getArray(string key,int *value,int n);
-	template void iniClass::getArray(string key,float *value,int n);
-	template void iniClass::getArray(string key,double *value,int n);
+	template void iniClass::getArray(const string &key,int *value,int n);
+	template void iniClass::getArray(const string &key,float *value,int n);
+	template void iniClass::getArray(const string &key,double *value,int n);
 }//namespace
 
 Ini::iniClass ini;//默认打开“000.ini”
