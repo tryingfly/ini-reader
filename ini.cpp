9171320935102392f3cb019e981e3f0ad448b0b2
tryingFly <tryingFly@fake.com>
2025-01-10 16:05:58 +0800

    支持section

--- ini.cpp
+++ ini.cpp
@@ -3,6 +3,7 @@ Copyright (C) 2024 YYP, Shanghai Jiao Tong University, under MIT License.
 <https://github.com/tryingfly/ini-reader>
 <https://zhuanlan.zhihu.com/p/685164920>
 
+<map>查找不要用value=map[key]，这样会插入不存在的key
 =====================================================*/
 #include"ini.h"
 #include<map>
@@ -13,15 +14,15 @@ Copyright (C) 2024 YYP, Shanghai Jiao Tong University, under MIT License.
 
 namespace Ini{
 	//按token分割
-	inline void splitString(string &str, vector<string> &substrings, char token){
+	inline void splitString(const string &str, vector<string> &substrings, char token){
 		string tmp;
 		stringstream ss(str);
 		while(getline(ss, tmp, token)){
 			substrings.push_back(tmp);
 		}
-	}
+	};
 	//去除空格、tab
-	void wipeWhite(string &str){
+	inline void wipeWhite(string &str){
 		size_t id=0;
 		while((id=str.find(' ',id)) != string::npos){
 			str.erase(id,1);
@@ -30,49 +31,51 @@ namespace Ini{
 		while((id=str.find('\t',id)) != string::npos){
 			str.erase(id,1);
 		}
-	}
+	};
 //==========================
 class iniClass::impClass{
 public:
-	impClass();
+	impClass(){};
 	impClass(const string &fileName);
-	ifstream f;
-	map<string,string>mp;
-	
-	void init();
-	double operator[](const string &key);
-	const string &getStr(const string &key);
+	bool open(const string &fileName,bool append);
 
+	map<string,string> defaultSec;//defaultSec是为了加快访问速度，在store中也存在
+	map<string,map<string,string>> store;
+	string file;
+
+	const map<string,string>&getSection(const string &section);
+
+	double getVal(const string &secName, const map<string,string> &sec, const string &key);
+	const string &getStr(const string &secName, const map<string,string> &sec, const string &key);
 	template<typename T,int n>
-	void getArray(const string &key,T (&value)[n]){getArray(key,value,n);};
+	void getArray(const string &secName, const map<string,string> &sec, const string &key,T (&value)[n]){getArray(key,value,n);};
 	template<typename T>
-	void getArray(const string &key,T *value,int n);
+	void getArray(const string &secName, const map<string,string> &sec, const string &key,T *value,int n);
 };
-	iniClass::impClass::impClass(){
-		#ifdef _WIN32
-		f.open("../../000.ini");
-		if(f.is_open()){fout.open("../../zzz.txt", ios::trunc);}
-		#else
-		f.open("../000.ini");
-		if(f.is_open()){fout.open("../zzz.txt", ios::trunc);}
-		#endif
-		if(!f.is_open()){
-			f.open("000.ini");
-			fout.open("zzz.txt", ios::trunc);
-		}
-		init();
-	}
 	iniClass::impClass::impClass(const string &fileName){
-		f.open(fileName);
-		init();
+		open(fileName,0);
 	}
-	void iniClass::impClass::init(){
+	bool iniClass::impClass::open(const string &fileName,bool append){
+		ifstream f;
+		f.open(fileName);
+		if(!f.good()){
+			stringstream ss;
+			ss<<"=== 未找到"<<fileName<<" !===";
+			throw runtime_error(ss.str());
+			return 0;
+		}
+		file=fileName;
+		if(!append){
+			defaultSec.clear();
+			store.clear();
+		}
+		auto *sec=&defaultSec;
+		string secName;
 		string line;
 		string preKey;
 		while(getline(f,line)){
 			size_t id=0;
-			//分号是ini的注释标志
-			id=line.find(";");
+			id=line.find(";");//分号是ini的注释标志
 			if(id!=string::npos){
 				line=line.substr(0,id);
 			}
@@ -80,13 +83,21 @@ public:
 			if(line.size()==0){
 				continue;
 			}
+			if(line.front()=='[' && line.back()==']'){
+				secName=line.substr(1,line.length()-2);
+				sec=&store[secName];//不存在会自动添加
+				preKey.clear();
+				// cout<<line.substr(1,line.length()-2)<<endl;
+				continue;
+			}
+			
 			id=line.find("=");
 			if(id==string::npos){//无“=”号，视为上一行的换行
 				if(preKey.size()>0){
-					auto &value=mp[preKey];
+					auto &value=(*sec)[preKey];
 					if(value[value.size()-1]!=',' && line[0]!=','){
 						stringstream ss;
-						ss<<"ini：跨行数组拼接，未发现逗号分割符！key="<<preKey;
+						ss<<"ini：跨行数组拼接，未发现逗号分割符！file="<<fileName<<"，section="<<secName<<"，key="<<preKey;
 						cout<<ss.str()<<endl;
 						throw runtime_error(ss.str());
 					}else{
@@ -96,52 +107,69 @@ public:
 			}else{//有“=”号，新键值对
 				string key,value;
 				key.assign(line,0,id);
+				if(sec->find(key) != sec->end()){
+					stringstream ss;
+					ss<<"ini：重复key！file="<<fileName<<"，section="<<secName<<"，key="<<preKey;
+					cout<<ss.str()<<endl;
+					throw runtime_error(ss.str());
+				}
 				value.assign(line,id+1,line.size()-id);
-				mp.insert(pair<string,string>(key,value));
+				sec->insert(pair<string,string>(key,value));
 				preKey=key;
 			}
 		}
+		return 1;
 	}
-	double iniClass::impClass::operator[](const string &key){
-		auto it=mp.find(key);
-		if(it!=mp.end()){
+	const map<string,string>& iniClass::impClass::getSection(const string &section){
+		auto it=store.find(section);
+		if(it!=store.end()){
+			return it->second;
+		}else{
+			stringstream ss;
+			ss<<"ini：未找到section！file="<<file<<"，section="<<section;
+			cout<<ss.str()<<endl;
+			throw runtime_error(ss.str());
+		}
+		return store[section];
+	}
+	double iniClass::impClass::getVal(const string &secName, const map<string,string> &sec,const string &key){
+		auto it=sec.find(key);
+		if(it!=sec.end()){
 			try{
 				return stod(it->second);
 			}catch(...){
 				stringstream ss;
-				ss<<"\" ini[\""<<key<<"\"] \" 值类型不匹配";
+				ss<<"ini:值类型不匹配！file="<<file<<"，section="<<secName<<"，key="<<key;
 				cout<<ss.str()<<endl;
 				throw runtime_error(ss.str());
 			}
 		}else{
 			stringstream ss;
-			ss<<"\" ini[\""<<key<<"\"] \" 找不到key";
+			ss<<"ini:找不到key！file="<<file<<"，section="<<secName<<"，key="<<key;
 			cout<<ss.str()<<endl;
 			throw runtime_error(ss.str());
 		}
-
-
 	};
-	const string &iniClass::impClass::getStr(const string &key){
-		auto it=mp.find(key);
-		if(it!=mp.end()){
+	const string &iniClass::impClass::getStr(const string &secName, const map<string,string> &sec,const string &key){
+		auto it=sec.find(key);
+		if(it!=sec.end()){
 			return it->second;
 		}else{
 			stringstream ss;
-			ss<<"\" ini[\""<<key<<"\"] \" 找不到key";
+			ss<<"ini:getStr找不到key！file="<<file<<"，section="<<secName<<"，key="<<key;
 			cout<<ss.str()<<endl;
 			throw runtime_error(ss.str());
 		}
 	};
 	template<typename T>
-	void iniClass::impClass::getArray(const string &key,T *value,int n){
-		auto it=mp.find(key);
-		if(it!=mp.end()){
+	void iniClass::impClass::getArray(const string &secName, const map<string,string> &sec,const string &key,T *value,int n){
+		auto it=sec.find(key);
+		if(it!=sec.end()){
 			std::vector<std::string> vs;
 			splitString(it->second,vs,',');
 			if(vs.size()<n){
 				stringstream ss;
-				ss<<"\"ini.getArray\" 数目不匹配，key="<<key;
+				ss<<"ini:getArray数目不匹配！file="<<file<<"，section="<<secName<<"，key="<<key;
 				cout<<ss.str()<<endl;
 				throw runtime_error(ss.str());
 			}
@@ -151,33 +179,50 @@ public:
 				}
 			}catch(...){
 				stringstream ss;
-				ss<<"\"ini.getArray\" 值类型不匹配，key="<<key;
+				ss<<"ini:getArray值类型不匹配！file="<<file<<"，section="<<secName<<"，key="<<key;
 				cout<<ss.str()<<endl;
 				throw runtime_error(ss.str());
 			}
 		}else{
 			stringstream ss;
-			ss<<"\"ini.getArray\" 找不到key="<<key;
+			ss<<"ini:getArray找不到key！file="<<file<<"，section="<<secName<<"，key="<<key;
 			cout<<ss.str()<<endl;
 			throw runtime_error(ss.str());
 		}
 	}
-//==========================
+	//==无section参数(或section为defaut)==
 	iniClass::iniClass():imp(*new impClass()){}
 	iniClass::iniClass(const string &fileName):imp(*new impClass(fileName)){}
+	bool iniClass::open(const string &fileName,bool append){return imp.open(fileName,append);}
+	double iniClass::getVal(const string &key){
+		return imp.getVal("默认", imp.defaultSec, key);
+	};
 	double iniClass::operator[](const string &key){
-		return imp[key];
+		return imp.getVal("默认", imp.defaultSec,key);
 	};
 	const string &iniClass::getStr(const string &key){
-		return imp.getStr(key);
+		return imp.getStr("默认", imp.defaultSec,key);
 	};
 	template<typename T>
 	void iniClass::getArray(const string &key,T *value,int n){
-		imp.getArray(key,value,n);
+		imp.getArray("默认", imp.defaultSec, key,value,n);
 	}
 	template void iniClass::getArray(const string &key,int *value,int n);
 	template void iniClass::getArray(const string &key,float *value,int n);
 	template void iniClass::getArray(const string &key,double *value,int n);
+	//==有section参数========
+	double iniClass::getVal(const string &section,const string &key){
+		return imp.getVal(section, imp.getSection(section), key);
+	};
+	const string &iniClass::getStr(const string &section,const string &key){
+		return imp.getStr(section, imp.getSection(section), key);
+	};
+	template<typename T>
+	void iniClass::getArray(const string &section,const string &key,T *value,int n){
+		imp.getArray(section, imp.getSection(section), key, value, n);
+	}
+	template void iniClass::getArray(const string &section,const string &key,int *value,int n);
+	template void iniClass::getArray(const string &section,const string &key,float *value,int n);
+	template void iniClass::getArray(const string &section,const string &key,double *value,int n);
 }//namespace
 
-Ini::iniClass ini;//默认打开“000.ini”
--- ini.h
+++ ini.h
@@ -5,8 +5,7 @@ Copyright (C) 2024 YYP, Shanghai Jiao Tong University, under MIT License.
 
 读取解析配置文件*.ini
 不要在实时循环使用，可以在进实时循环前先找变量赋值
-不能在全局变量使用
-可以在类构造函数中使用(main之前执行)，但该类不能实例化全局变量
+
 #include"ini.h"
 =====================================================*/
 #pragma once
@@ -17,18 +16,29 @@ namespace Ini{
 //不要在实时循环使用
 class iniClass{
 public:
-	iniClass();//不要在实时循环使用
-	iniClass(const string &fileName);//不要在实时循环使用
-	double operator[](const string &key);//不要在实时循环使用
+	iniClass();
+	iniClass(const string &fileName);//等价于无参构造+open，
+	bool open(const string &fileName, bool append=0);//append=1用于打开文件并在原有基础上键值对
+
+	//--不存在section的独立数据获取--
+	double getVal(const string &key);       //不要在实时循环使用
+	double operator[](const string &key);   //不要在实时循环使用
 	const string &getStr(const string &key);//不要在实时循环使用
-	template<typename T,int n>
-	void getArray(const string &key,T (&value)[n]){getArray(key,value,n);};//不要在实时循环使用
 	template<typename T>
-	void getArray(const string &key,T *value,int n);//不要在实时循环使用
-private:
+	void getArray(const string &key, T*value, int n); //不要在实时循环使用
+	template<typename T,int n>
+	void getArray(const string &key, T(&value)[n]){getArray(key,value,n);};//不要在实时循环使用
+
+	//--有section参数--
+	double getVal(const string &section, const string &key);       //不要在实时循环使用
+	const string &getStr(const string &section, const string &key);//不要在实时循环使用
+	template<typename T>
+	void getArray(const string &section, const string &key, T*value, int n);//不要在实时循环使用
+	template<typename T,int n>
+	void getArray(const string &section, const string &key, T(&value)[n]){getArray(key,value,n);};//不要在实时循环使用
+protected:
 	class impClass;
 	impClass&imp;
 };
-}//namespace
 
-extern Ini::iniClass ini;//已实例化，默认打开“000.ini”
+}//namespace
